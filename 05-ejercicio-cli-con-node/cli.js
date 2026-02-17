466334258bc5931ea72c506d2635e438e4269d30
madeval <alvarez.fing@gmail.com>
2026-02-16 23:39:19 -0300

    Refactorizar manejo de permisos y simplificar lógica de filtrado en CLI

--- 05-ejercicio-cli-con-node/cli.js
+++ 05-ejercicio-cli-con-node/cli.js
@@ -10,14 +10,26 @@ const orden = process.argv.find(arg => arg === '--asc' || arg === '--desc') ?? '
 const searchFiles = process.argv.find(arg => arg === '--files') ?? false;
 const searchDirs = process.argv.find(arg => arg === '--folders') ?? false;
 
+/* La manera de revisar si el usuario tiene permisos de lectura es a partir de la propiedad `permission` del objeto `process` */
+if (!process.permission){
+  console.log('[ERROR]: Los permisos deben ser activados\nuse el flag [--permission]') // <- Podemos dejar un mensaje más descriptivo
+  process.exit(1)
+}
+
+/* Y ahora que el usuario agregó el flag de permisos, debemos revisar que el usuario le haya dado permisos de lectura al directorio al cual queremos acceder */
+if (!process.permission.has('fs.read', dir)) {
+  console.log(`[ERROR]: Permiso denegado, use el flag [--permission --allow-fs-read=[directorio_a_leer]] \nPor ejemplo: [node --permission --allow-fs-read=${dir} cli.js ${dir}]`)
+  process.exit(1)
+}
+
 // Leer nombres del directorio y comprueba si tienes permiso de lectura del directorio
 let archivos
 try{
     archivos = await readdir(dir);
 }catch (error) {
-    console.error('ERROR: No tienes permiso de lectura del directorio');
-    process.exit(1);
-}
+    // Si falla reddir, no será por un error de permisos (eso lo evaluamos antes), sino porque el directorio no existe
+    console.error(`ERROR: No se pudo leer el directorio ${dir}`);
+    process.exit(1);}
 
 // Ordenar los archivos
 archivos.sort((a, b) => {
@@ -46,13 +58,13 @@ const entries = await Promise.all(
 entries.forEach((entry) => {
     const icon = entry.isDirectory ? '📁' : '📄';
     const size = entry.isDirectory ? ' -' : ` ${entry.size.toFixed(2)} KB`;
-    if(entry.isDirectory && searchDirs) {
-        console.log(`${icon} ${entry.name.padEnd(20)} ${size}`);
-    }
-    else if(!entry.isDirectory && searchFiles) {
-        console.log(`${icon} ${entry.name.padEnd(20)} ${size}`);
-    }
-    else if (!searchFiles && !searchDirs) {
+    
+    // Si tenemos muchos `if` que devuelven lo mismo, podemos simplificarlo de esta manera
+    const shouldShow = (!searchFiles && !searchDirs) || 
+                       (entry.isDirectory && searchDirs) || 
+                       (!entry.isDirectory && searchFiles);
+    
+    if (shouldShow) {
         console.log(`${icon} ${entry.name.padEnd(20)} ${size}`);
     }
 })
